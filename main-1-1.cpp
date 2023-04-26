51828a91cfe721765c70acb15ff280a44ee93012
Christian <christian.goiak@outlook.com>
2023-04-26 14:35:06 +0930

    sdfg

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
+
 int main()
 {
+    Appliance();
+    Appliance(int powerRating); // creates an Appliance with a power rating
+
+    int powerRating;
+    bool isOn;
+
+    void turnOn();
+    void turnOff();
+    double getPowerConsumption();
     return 0;
 }
