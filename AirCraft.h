aeb54a0ca7f5229673bf20f5e5372705a8f6041a
Christian <christian.goiak@outlook.com>
2023-04-27 10:19:53 +0930

    jfgh

--- AirCraft.h
+++ AirCraft.h
@@ -2,19 +2,17 @@
 #include <string>
 
 class AirCraft();
-{
-    AirCraft(int w); // creates an Aircraft with weight w
+
 private:
-    {
-        int weight;          // the weight of AirCraft
-        float fuel;          // fuel percentage, initially 100%
-        int numberOfFlights; // initially 0
-    }
+{
+    int weight;          // the weight of AirCraft
+    float fuel;          // fuel percentage, initially 100%
+    int numberOfFlights; // initially 0
+}
 
 public:
-    {
-        void refuel();                       // Resets fuel back to 100%
-        void fly(int headwind, int minutes); // headwind in km/h and minutes (time flying)
-    }
-    return 0
+{
+    void refuel();                       // Resets fuel back to 100%
+    void fly(int headwind, int minutes); // headwind in km/h and minutes (time flying)
 }
+return 0
